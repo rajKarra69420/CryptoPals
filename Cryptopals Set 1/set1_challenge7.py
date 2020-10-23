e181337d99ec14a8f75741714f341e7caa64ffd4
rajKarra69420 <60681803+rajKarra69420@users.noreply.github.com>
2020-10-23 02:17:28 -0400

    Add files via upload

--- Cryptopals Set 1/set1_challenge7.py
+++ Cryptopals Set 1/set1_challenge7.py
@@ -1,8 +1,6 @@
-#https://en.wikipedia.org/wiki/Block_cipher_mode_of_operation#Electronic_codebook_(ECB)
-
-import pycryptodome
+from Crypto.Cipher import AES
 import base64
 
 f = open("set1_challenge7_encrypted.txt", "r")
-p = new AES.new(b'YELLOW SUBMARINE', AES.MODE_ECB).decrypt(base64.b64decode(f.read()))
-
+p = AES.new(b'YELLOW SUBMARINE', AES.MODE_ECB).decrypt(base64.b64decode(f.read()))
+print(p)
